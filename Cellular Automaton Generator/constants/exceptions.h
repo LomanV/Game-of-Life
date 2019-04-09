708857bf9a553a0f42704669e0194a8dc118e06c
Vezin Lomàn <loman.vezin@gmail.com>
2019-04-09 10:18:49 +0200

    various

--- Cellular Automaton Generator/constants/exceptions.h
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-namespace excptn {
-	const std::out_of_range BAD_VECTOR_ACCESS("Could not access Vector3D's i-th coordinates for i≠0,1");
-	const std::out_of_range ZERO_VECTOR_UNITARY("Could not normalise zero vector");
-}
